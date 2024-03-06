dd5f04998e4deb9759869782e7a3c9579d624923
pramod1712 <pramodsarathy17@gmail.com>
2024-03-06 18:52:19 +0530

    Adding Dockerfile and main directories

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My SRN is PES1UG21CS427\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
